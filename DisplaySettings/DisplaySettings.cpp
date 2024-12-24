7e4644abe6e47bd802052593e6826eaf7cccaf58
hgfell683 <107510770+hgfell683@users.noreply.github.com>
2024-12-24 08:36:33 -0500

    DELIA-66702: Documentation update
    Reason for change: Documentation Update
    Test Procedure:
    Risks: low
    Priority: P1
    
    Signed-off-by:Hayden Gfeller Hayden_Gfeller@comcast.com

--- DisplaySettings/DisplaySettings.cpp
+++ DisplaySettings/DisplaySettings.cpp
@@ -83,7 +83,7 @@ using namespace std;
 
 #define API_VERSION_NUMBER_MAJOR 2
 #define API_VERSION_NUMBER_MINOR 0
-#define API_VERSION_NUMBER_PATCH 1
+#define API_VERSION_NUMBER_PATCH 2
 
 static bool isCecEnabled = false;
 static bool isResCacheUpdated = false;

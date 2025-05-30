315831f2d3760f96536d2c48f0436d3d82bedcec
kgnanagurusamy <kgnanagurusamy@synamedia.com>
2025-05-30 15:07:39 +0530

    Added mfr library package

--- cmake/FindMFR.cmake
+++ cmake/FindMFR.cmake
@@ -7,13 +7,13 @@
 find_package(PkgConfig)
 
 find_library(MFR_LIBRARY NAMES RDKMfrLib)
-find_path(MFR_INCLUDE_DIR NAMES mfr_temperature.h PATH_SUFFIXES mfr/include)
+find_path(MFR_INCLUDE_DIR NAMES mfr_temperature.h PATH_SUFFIXES /usr/include/mfr/include)
 
 set(MFR_INCLUDE_DIR ${MFR_INCLUDE_DIR})
 set(MFR_INCLUDE_DIR ${MFR_INCLUDE_DIR} CACHE PATH "Path to MFR include")
 
-include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(MFR DEFAULT_MSG MFR_INCLUDE_DIR MFR_LIBRARY)
+#include(FindPackageHandleStandardArgs)
+#FIND_PACKAGE_HANDLE_STANDARD_ARGS(MFR DEFAULT_MSG MFR_INCLUDE_DIR MFR_LIBRARY)
 
 mark_as_advanced(
     MFR_INCLUDE_DIR

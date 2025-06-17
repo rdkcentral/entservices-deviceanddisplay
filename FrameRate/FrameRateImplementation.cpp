e81ae013796fa85c8d0b39644108612709e3f697
mukesh972 <mukesh.m@tataelxsi.co.in>
2025-06-17 18:30:24 +0530

    RDKEMW-1012: COMRPC FrameRate plugin and test app
    Replaced ERROR_GENERAL with ERROR_NOT_SUPPORTED if video devices are not available.
    
    Signed-off-by: mukesh972 <mukesh.m@tataelxsi.co.in>

--- FrameRate/FrameRateImplementation.cpp
+++ FrameRate/FrameRateImplementation.cpp
@@ -276,7 +276,7 @@ namespace WPEFramework
                 if (videoDevices.size() == 0)
                 {
                     LOGERR("No video devices available.");
-                    return Core::ERROR_GENERAL;
+                    return Core::ERROR_NOT_SUPPORTED;
                 }
 
                 char sFramerate[32] = {0};
@@ -316,7 +316,7 @@ namespace WPEFramework
                 if (videoDevices.size() == 0)
                 {
                     LOGERR("No video devices available.");
-                    return Core::ERROR_GENERAL;
+                    return Core::ERROR_NOT_SUPPORTED;
                 }
                 device::VideoDevice& device = videoDevices.at(0);
                 if (!device.getFRFMode(&autoFRMMode))
@@ -392,7 +392,7 @@ namespace WPEFramework
                 if (videoDevices.size() == 0)
                 {
                     LOGERR("No video devices available.");
-                    return Core::ERROR_GENERAL;
+                    return Core::ERROR_NOT_SUPPORTED;
                 }
                 device::VideoDevice& device = videoDevices.at(0);
                 if (!device.setDisplayframerate(sFramerate.c_str()))
@@ -432,7 +432,7 @@ namespace WPEFramework
                 if (videoDevices.size() == 0)
                 {
                     LOGERR("No video devices available.");
-                    return Core::ERROR_GENERAL;
+                    return Core::ERROR_NOT_SUPPORTED;
                 }
                 device::VideoDevice& device = videoDevices.at(0);
                 if (!device.setFRFMode(frmmode))

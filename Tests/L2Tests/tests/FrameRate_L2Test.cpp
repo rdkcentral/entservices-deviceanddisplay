97c59a4764e201edd5e9280358774cac82ee6a0f
mukesh972 <mukesh.m@tataelxsi.co.in>
2025-06-27 15:06:20 +0530

    test-l2-fr
    Signed-off-by: mukesh972 <mukesh.m@tataelxsi.co.in>

--- Tests/L2Tests/tests/FrameRate_L2Test.cpp
+++ Tests/L2Tests/tests/FrameRate_L2Test.cpp
@@ -773,7 +773,7 @@ TEST_F(FrameRate_L2test, SetDisplayFrameRateUsingJsonrpc) {
     params["FrameRate"] = "3840x2160px48";
     status = InvokeServiceMethod(FrameRate_CALLSIGN, "setDisplayFrameRate", params, result);
     /* API returns ERROR_NOT_SUPPORTED for TV PROFILE so changed to FALSE */
-    EXPECT_FALSE(result.Boolean());
+    EXPECT_FALSE(result["result"].Boolean());
 }
 
 /************Test case Details **************************
@@ -851,7 +851,7 @@ TEST_F(FrameRate_L2test, SetFrmModeUsingJsonrpc) {
     params["frmmode"] = 0;
     status = InvokeServiceMethod(FrameRate_CALLSIGN, "setFrmMode", params, result);
     /* API returns ERROR_NOT_SUPPORTED for TV PROFILE so changed to FALSE */
-    EXPECT_FALSE(result.Boolean());
+    EXPECT_FALSE(result["result"].Boolean());
 }
 
 /************Test case Details **************************

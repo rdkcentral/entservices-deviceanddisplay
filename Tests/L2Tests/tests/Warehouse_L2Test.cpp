2572a055f3b40fe88e908b971cbdf4b7873c54f6
Sankalp Maneshwar <sankalpmaneshwar46@outlook.com>
2025-07-09 15:32:56 +0530

    Update Warehouse_L2Test.cpp
    Signed-off-by: Sankalp Maneshwar <sankalpmaneshwar46@outlook.com>

--- Tests/L2Tests/tests/Warehouse_L2Test.cpp
+++ Tests/L2Tests/tests/Warehouse_L2Test.cpp
@@ -1595,6 +1595,7 @@ TEST_F(Warehouse_L2Test, Warehouse_False_Clear_ResetDevice)
 {
     JSONRPC::LinkType<Core::JSON::IElement> jsonrpc(WAREHOUSE_CALLSIGN, WAREHOUSEL2TEST_CALLSIGN);
     StrictMock<AsyncHandlerMock_Warehouse> async_handler;
+    uint32_t signalled = WAREHOUSEL2TEST_STATE_INVALID;
     uint32_t status = Core::ERROR_GENERAL;
     JsonObject params;
     JsonObject result;
@@ -1607,6 +1608,22 @@ TEST_F(Warehouse_L2Test, Warehouse_False_Clear_ResetDevice)
 
     /* Activate plugin in TEST_F*/
     status = ActivateService("org.rdk.Warehouse");
+    EXPECT_EQ(Core::ERROR_NONE, status);
+
+    message = "{\"success\":true,\"error\":\"\"}";
+    expected_status.FromString(message);
+    EXPECT_CALL(async_handler, resetDone(MatchRequest(expected_status)))
+        .WillOnce(Invoke(this, &Warehouse_L2Test::resetDone));
+
+    /* errorCode and errorDescription should not be set */
+    EXPECT_FALSE(result.HasLabel("errorCode"));
+    EXPECT_FALSE(result.HasLabel("errorDescription"));
+
+    /* Register for resetDone event. */
+    status = jsonrpc.Subscribe<JsonObject>(JSON_TIMEOUT,
+        _T("resetDone"),
+        &AsyncHandlerMock_Warehouse::resetDone,
+        &async_handler);
     EXPECT_EQ(Core::ERROR_NONE, status);
 
     EXPECT_CALL(*p_wrapsImplMock, v_secure_system(::testing::_, ::testing::_))
@@ -1622,6 +1639,12 @@ TEST_F(Warehouse_L2Test, Warehouse_False_Clear_ResetDevice)
     status = InvokeServiceMethod("org.rdk.Warehouse.1", "resetDevice", params, result);
     EXPECT_EQ(Core::ERROR_NONE, status);
     EXPECT_TRUE(result["success"].Boolean());
+
+    signalled = WaitForRequestStatus(JSON_TIMEOUT, WAREHOUSEL2TEST_RESETDONE);
+    EXPECT_TRUE(signalled & WAREHOUSEL2TEST_RESETDONE);
+
+    /* Unregister for events. */
+    jsonrpc.Unsubscribe(JSON_TIMEOUT, _T("resetDone"));
 }
 
 /********************************************************

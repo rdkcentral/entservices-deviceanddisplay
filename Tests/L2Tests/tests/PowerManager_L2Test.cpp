1908b2fb123ab02adfab835b1c331bb3593f35fd
KaleeswaranGnanagurusamy <151117743+KaleeswaranGnanagurusamy@users.noreply.github.com>
2025-08-22 16:33:04 +0530

    Update PowerManager_L2Test.cpp
    Signed-off-by: KaleeswaranGnanagurusamy <151117743+KaleeswaranGnanagurusamy@users.noreply.github.com>

--- Tests/L2Tests/tests/PowerManager_L2Test.cpp
+++ Tests/L2Tests/tests/PowerManager_L2Test.cpp
@@ -28,9 +28,9 @@
 #include "deepSleepMgr.h"
 #include "PowerManagerMock.h"
 #include "PowerManagerHalMock.h"
+#if 0
 #include "MfrMock.h"
 
-#if 0
 #define JSON_TIMEOUT   (1000)
 #define TEST_LOG(x, ...) fprintf(stderr, "\033[1;32m[%s:%d](%s)<PID:%d><TID:%d>" x "\n\033[0m", __FILE__, __LINE__, __FUNCTION__, getpid(), gettid(), ##__VA_ARGS__); fflush(stderr);
 #define SYSTEM_CALLSIGN  _T("org.rdk.PowerManager.1")

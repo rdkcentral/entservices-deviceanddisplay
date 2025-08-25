9bded02082490ef8cf76bae8f04d517a500a6304
KaleeswaranGnanagurusamy <kgnanagurusamy@synamedia.com>
2025-08-25 21:13:41 +0530

    RDKEMW-7092: Resolved the L1 and L2 test failure

--- Tests/L2Tests/tests/PowerManager_L2Test.cpp
+++ Tests/L2Tests/tests/PowerManager_L2Test.cpp
@@ -482,7 +482,6 @@ void PowerManager_L2Test::Test_OvertempGraceInterval( Exchange::IPowerManager* P
 /* COM-RPC tests */
 void PowerManager_L2Test::Test_TemperatureThresholds( Exchange::IPowerManager* PowerManagerPlugin )
 {
-    uint32_t signalled = POWERMANAGERL2TEST_STATE_INVALID;
     uint32_t status = Core::ERROR_GENERAL;
     float high = 100, critical = 110;
     TEST_LOG("\n################## Running Test_TemperatureThresholds Test #################\n");
@@ -884,6 +883,7 @@ TEST_F(PowerManager_L2Test,DeepSleepFailure)
     Core::ProxyType<RPC::CommunicatorClient> mClient_PowerManager;
     PluginHost::IShell *mController_PowerManager;
     uint32_t signalled = POWERMANAGERL2TEST_STATE_INVALID;
+    uint32_t deepSleepTimeout = 10;
 
     TEST_LOG("Creating mEngine_PowerManager");
     mEngine_PowerManager = Core::ProxyType<RPC::InvokeServerType<1, 0, 4>>::Create();
@@ -914,7 +914,7 @@ TEST_F(PowerManager_L2Test,DeepSleepFailure)
 
             if (PowerManagerPlugin)
             {
-                uint32_t status = PowerManagerPlugin->SetDeepSleepTimer(10);
+                uint32_t status = PowerManagerPlugin->SetDeepSleepTimer(deepSleepTimeout);
                 EXPECT_EQ(status, Core::ERROR_NONE);
 
                 signalled = mNotification.WaitForRequestStatus(JSON_TIMEOUT * 3, POWERMANAGERL2TEST_SYSTEMSTATE_PRECHANGE);
@@ -935,8 +935,8 @@ TEST_F(PowerManager_L2Test,DeepSleepFailure)
                 EXPECT_CALL(PowerManagerHalMock::Mock(), PLAT_DS_SetDeepSleep(::testing::_, ::testing::_, ::testing::_))
                     .Times(5)
                     .WillRepeatedly(::testing::Invoke(
-                        [](uint32_t deep_sleep_timeout, bool* isGPIOWakeup, bool networkStandby) {
-                            EXPECT_EQ(deep_sleep_timeout, 10);
+                        [&](uint32_t deep_sleep_timeout, bool* isGPIOWakeup, bool networkStandby) {
+                            EXPECT_EQ(deep_sleep_timeout, deepSleepTimeout);
                             EXPECT_TRUE(nullptr != isGPIOWakeup);
                             EXPECT_EQ(networkStandby, false);
                             // Simulate timer wakeup
@@ -1148,6 +1148,7 @@ TEST_F(PowerManager_L2Test,DeepSleepInvalidWakeup)
     Core::ProxyType<RPC::CommunicatorClient> mClient_PowerManager;
     PluginHost::IShell *mController_PowerManager;
     uint32_t signalled = POWERMANAGERL2TEST_STATE_INVALID;
+    uint32_t deepSleepTimeout = 10;
 
     TEST_LOG("Creating mEngine_PowerManager");
     mEngine_PowerManager = Core::ProxyType<RPC::InvokeServerType<1, 0, 4>>::Create();
@@ -1178,7 +1179,7 @@ TEST_F(PowerManager_L2Test,DeepSleepInvalidWakeup)
 
             if (PowerManagerPlugin)
             {
-                uint32_t status = PowerManagerPlugin->SetDeepSleepTimer(10);
+                uint32_t status = PowerManagerPlugin->SetDeepSleepTimer(deepSleepTimeout);
                 EXPECT_EQ(status, Core::ERROR_NONE);
 
                 signalled = mNotification.WaitForRequestStatus(JSON_TIMEOUT * 3, POWERMANAGERL2TEST_SYSTEMSTATE_PRECHANGE);
@@ -1199,8 +1200,8 @@ TEST_F(PowerManager_L2Test,DeepSleepInvalidWakeup)
 
                 EXPECT_CALL(PowerManagerHalMock::Mock(), PLAT_DS_SetDeepSleep(::testing::_, ::testing::_, ::testing::_))
                     .WillOnce(::testing::Invoke(
-                        [](uint32_t deep_sleep_timeout, bool* isGPIOWakeup, bool networkStandby) {
-                            EXPECT_EQ(deep_sleep_timeout, 10);
+                        [&](uint32_t deep_sleep_timeout, bool* isGPIOWakeup, bool networkStandby) {
+                            EXPECT_EQ(deep_sleep_timeout, deepSleepTimeout);
                             EXPECT_TRUE(nullptr != isGPIOWakeup);
                             EXPECT_EQ(networkStandby, false);
                             // Simulate timer wakeup

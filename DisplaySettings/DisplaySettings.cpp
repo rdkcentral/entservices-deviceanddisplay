182c824ce1f09f171d0ef8e97054dc38d9a38ede
Dosakaya <110212899+Dosakaya@users.noreply.github.com>
2025-09-13 22:39:56 +0530

    Update DisplaySettings.cpp
    Signed-off-by: Dosakaya <110212899+Dosakaya@users.noreply.github.com>

--- DisplaySettings/DisplaySettings.cpp
+++ DisplaySettings/DisplaySettings.cpp
@@ -385,7 +385,7 @@ namespace WPEFramework {
             catch(const device::Exception& err)
             {
                 LOGWARN("Audio Port : AudioPortsReInitialize failed\n");
-                //LOG_DEVICE_EXCEPTION0();
+                LOG_DEVICE_EXCEPTION0();
             }
         }
      

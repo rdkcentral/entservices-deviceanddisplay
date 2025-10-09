e3cf5ab8be03c28ede97919afb6b895d9fab1a71
HarshiniRavichandran <166796069+HarshiniRavichandran@users.noreply.github.com>
2025-10-09 13:12:49 +0530

    Update DeviceDiagnosticsImplementation.cpp
    Signed-off-by: HarshiniRavichandran <166796069+HarshiniRavichandran@users.noreply.github.com>

--- DeviceDiagnostics/DeviceDiagnosticsImplementation.cpp
+++ DeviceDiagnostics/DeviceDiagnosticsImplementation.cpp
@@ -87,10 +87,7 @@ namespace WPEFramework
             m_pollThreadRun = 0;
             m_AVDecoderStatusLock.unlock();
             m_avDecoderStatusCv.notify_one();
-			if (m_AVPollThread.joinable())
-			{
-                m_AVPollThread.join();
-			}
+            m_AVPollThread.join();
             EssRMgrDestroy(m_EssRMgr);
 #endif
             DeviceDiagnosticsImplementation::_instance = nullptr;

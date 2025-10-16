07e2114ac6050c2c70f5b7d36a1e857dc0ac020e
preeja33 <preeja_raveendran@comcast.com>
2025-10-16 13:33:23 +0530

    Update FirmwareVersion.cpp
    Signed-off-by: preeja33 <preeja_raveendran@comcast.com>

--- DeviceInfo/Implementation/FirmwareVersion.cpp
+++ DeviceInfo/Implementation/FirmwareVersion.cpp
@@ -45,6 +45,39 @@ namespace Plugin {
 
             return result;
         }
+ 
+      uint32_t GetStringRegex(const string& input, const std::regex& regex) {
+            uint32_t result = Core::ERROR_GENERAL;
+            std::smatch sm;
+
+            if ((std::regex_match(input, sm, regex)) && (sm.size() > 1)) {
+                result = Core::ERROR_NONE;
+            }
+            return result;
+        }
+
+       static bool RunCommand(const std::string& command, std::string& result) {
+           FILE* fp = popen(command.c_str(), "r");
+           if (!fp) {
+              return false;
+           }
+
+           std::ostringstream oss;
+           char buffer[64];
+           while (fgets(buffer, sizeof(buffer), fp) != nullptr) {
+              oss << buffer;
+           }
+
+           pclose(fp);
+           result = oss.str();
+
+           if (result.empty()) {
+             return false;
+           }
+
+           return true;
+       }
+
     }
 
     SERVICE_REGISTRATION(FirmwareVersion, 1, 0);
@@ -52,6 +85,15 @@ namespace Plugin {
     Core::hresult FirmwareVersion::Imagename(string& imagename) const
     {
         return GetFileRegex(_T("/version.txt"), std::regex("^imagename:([^\\n]+)$"), imagename);
+    }
+
+    Core::hresult FirmwareVersion::Pdri(string& pdri) const
+    {
+         if (RunCommand("/usr/bin/mfr_util --PDRIVersion", pdri)) {
+             return GetStringRegex(pdri, std::regex("failed"));
+         }
+        
+        return Core::ERROR_NONE;
     }
 
     Core::hresult FirmwareVersion::Sdk(string& sdk) const

cd230e612eba0b650266c68a2075f8ed5f300eac
Sankalp Maneshwar <sankalpmaneshwar46@outlook.com>
2025-12-10 18:11:29 +0530

    Update test_PowerManager.cpp
    Signed-off-by: Sankalp Maneshwar <sankalpmaneshwar46@outlook.com>

--- Tests/L1Tests/tests/test_PowerManager.cpp
+++ Tests/L1Tests/tests/test_PowerManager.cpp
@@ -478,7 +478,7 @@ TEST_F(TestPowerManager, GetCoreTemperature)
     EXPECT_EQ(status, Core::ERROR_NONE);
 }
 
-TEST_F(TestPowerManager, DISABLED_PowerModePreChangeAck)
+TEST_F(TestPowerManager, PowerModePreChangeAck)
 {
     EXPECT_CALL(*p_powerManagerHalMock, PLAT_API_SetPowerState(::testing::_))
         .WillOnce(::testing::Invoke(

8cafbe0550dbe4ea499da8f1300fac6fa293a6a2
grandhi_santoshkumar <grandhi_santoshkumar@comcast.com>
2026-01-22 14:43:52 +0000

    RDKEMW-6176: Devicesettings plugin implementation.
    Reason for change: Devicesettings plugin implementation with split implementation.
    Test Procedure: refer RDKEMW-6176
    Risks: High
    Signed-off-by:gsanto722 <grandhi_santoshkumar@comcast.com>

---
+++ DeviceSettings/DeviceSettings.cpp
@@ -0,0 +1,211 @@
+/**
+* If not stated otherwise in this file or this component's LICENSE
+* file the following copyright and licenses apply:
+*
+* Copyright 2024 RDK Management
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+**/
+
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+//#include "libIBus.h"
+#include <string.h> 
+#include <iostream>
+#include <fstream>
+
+#include "DeviceSettings.h"
+#include <interfaces/IConfiguration.h>
+
+
+namespace WPEFramework {
+
+namespace Plugin
+{
+    SERVICE_REGISTRATION(DeviceSettings, API_VERSION_MAJOR, API_VERSION_MINOR, API_VERSION_PATCH);
+
+    namespace {
+        static Metadata<DeviceSettings> metadata(
+            // Version
+            API_VERSION_MAJOR, API_VERSION_MINOR, API_VERSION_PATCH,
+            // Preconditions
+            {},
+            // Terminations
+            {},
+            // Controls
+            {}
+        );
+    }
+
+    DeviceSettings::DeviceSettings()
+        : mConnectionId(0)
+        , mService(nullptr)
+#ifndef USE_LEGACY_INTERFACE
+        , _mDeviceSettings(nullptr)
+#endif
+        , mNotificationSink(this)
+
+    {
+        #if (defined(RDK_LOGGER_ENABLED) || defined(DSMGR_LOGGER_ENABLED))
+
+        const char* PdebugConfigFile = NULL;
+        const char* DSMGR_DEBUG_ACTUAL_PATH    = "/etc/debug.ini";
+        const char* DSMGR_DEBUG_OVERRIDE_PATH  = "/opt/debug.ini";
+
+        /* Init the logger */
+        if (access(DSMGR_DEBUG_OVERRIDE_PATH, F_OK) != -1 ) {
+            PdebugConfigFile = DSMGR_DEBUG_OVERRIDE_PATH;
+        }
+        else {
+            PdebugConfigFile = DSMGR_DEBUG_ACTUAL_PATH;
+        }
+
+        if (rdk_logger_init(PdebugConfigFile) == 0) {
+            b_rdk_logger_enabled = 1;
+        }
+
+#endif
+
+    }
+
+    
+    DeviceSettings::~DeviceSettings()
+    {
+    }
+    const string DeviceSettings::Initialize(PluginHost::IShell * service)
+    {
+        string message = "";
+        ENTRY_LOG;
+        ASSERT(service != nullptr);
+        ASSERT(mService == nullptr);
+        ASSERT(mConnectionId == 0);
+#ifdef USE_LEGACY_INTERFACE
+        ASSERT(_mDeviceSettingsFPD == nullptr);
+        ASSERT(_mDeviceSettingsHDMIIn == nullptr);
+#else
+        ASSERT(_mDeviceSettings == nullptr);
+#endif
+        mService = service;
+        mService->AddRef();
+
+        mService->Register(mNotificationSink.baseInterface<RPC::IRemoteConnection::INotification>());
+        mService->Register(mNotificationSink.baseInterface<PluginHost::IShell::ICOMLink::INotification>());
+
+#ifdef USE_LEGACY_INTERFACE
+        // Get IDeviceSettingsFPD interface.
+        _mDeviceSettingsFPD = service->Root<Exchange::IDeviceSettingsFPD>(mConnectionId, RPC::CommunicationTimeOut, _T("DeviceSettingsFPDImp"));
+
+
+        if (_mDeviceSettingsFPD == nullptr) {
+            SYSLOG(Logging::Startup, (_T("DeviceSettings::Initialize: Failed to initialise DeviceSettings plugin")));
+            message = _T("DeviceSettings plugin could not be initialised");
+            LOGERR("Failed to get IDeviceSettingsFPD interface");
+        } else {
+            LOGINFO("DeviceSettingsFPD interface obtained successfully");
+            #if 0
+            // Use QueryInterface to get HDMI interface from the same object instance
+            _mDeviceSettingsHDMIIn = _mDeviceSettingsFPD->QueryInterface<Exchange::IDeviceSettingsHDMIIn>();
+
+            if (_mDeviceSettingsHDMIIn == nullptr) {
+                SYSLOG(Logging::Startup, (_T("DeviceSettings::Initialize: Failed to query HDMI interface from same object")));
+                message = _T("DeviceSettings HDMI interface could not be queried");
+                LOGERR("Failed to QueryInterface for IDeviceSettingsHDMIIn from same object");
+            } else {
+                LOGINFO("DeviceSettingsHDMIIn interface queried successfully");
+            }
+            #endif
+        }
+        // Get IDeviceSettingsHDMIIn interface from 
+        _mDeviceSettingsHDMIIn = service->Root<Exchange::IDeviceSettingsHDMIIn>(mConnectionId, RPC::CommunicationTimeOut, _T("DeviceSettingsHdmiInImp"));
+
+        if (_mDeviceSettingsHDMIIn == nullptr) {
+            SYSLOG(Logging::Startup, (_T("DeviceSettings::Initialize: Failed to get IDeviceSettingsHDMIIn interface")));
+            message = _T("DeviceSettings HDMI interface could not be queried");
+            LOGERR("Failed to get IDeviceSettingsHDMIIn interface");
+        } else {
+            LOGINFO("DeviceSettingsHDMIIn interface queried successfully");
+        }
+#else
+        // Get the unified interface that provides both FPD and HDMI functionality
+        _mDeviceSettings = service->Root<Exchange::IDeviceSettings>(mConnectionId, RPC::CommunicationTimeOut, _T("DeviceSettingsImp"));
+
+        if (_mDeviceSettings == nullptr) {
+            SYSLOG(Logging::Startup, (_T("DeviceSettings::Initialize: Failed to initialise DeviceSettings plugin")));
+            message = _T("DeviceSettings plugin could not be initialised");
+            LOGERR("Failed to get IDeviceSettings interface");
+        } else {
+            LOGINFO("DeviceSettingsImp initialized successfully");
+        }
+#endif
+        if (0 != message.length()) {
+            Deinitialize(service);
+        }
+        EXIT_LOG;
+
+        // On success return empty, to indicate there is no error text.
+        return (message);
+    }
+
+    void DeviceSettings::Deinitialize(PluginHost::IShell* service VARIABLE_IS_NOT_USED)
+    {
+        ENTRY_LOG;
+        if (mService != nullptr) {
+            ASSERT(mService == service);
+            mService->Unregister(mNotificationSink.baseInterface<RPC::IRemoteConnection::INotification>());
+            mService->Unregister(mNotificationSink.baseInterface<PluginHost::IShell::ICOMLink::INotification>());
+#ifdef USE_LEGACY_INTERFACE
+            _mDeviceSettingsFPD = nullptr;
+            _mDeviceSettingsHDMIIn = nullptr;
+#else
+            _mDeviceSettings = nullptr;
+#endif
+            mService->Release();
+            mService = nullptr;
+            mConnectionId = 0;
+            SYSLOG(Logging::Shutdown, (string(_T("DeviceSettings de-initialised"))));
+        }
+        EXIT_LOG;
+    }
+
+    string DeviceSettings::Information() const
+    {
+        ENTRY_LOG;
+        // No additional info to report.
+        EXIT_LOG;
+
+        return (string());
+    }
+
+    void DeviceSettings::Deactivated(RPC::IRemoteConnection* connection)
+    {
+        ENTRY_LOG;
+        // This can potentially be called on a socket thread, so the deactivation (which in turn kills this object) must be done
+        // on a separate thread. Also make sure this call-stack can be unwound before we are totally destructed.
+        if (mConnectionId == connection->Id()) {
+            ASSERT(mService != nullptr);
+            Core::IWorkerPool::Instance().Submit(PluginHost::IShell::Job::Create(mService, PluginHost::IShell::DEACTIVATED, PluginHost::IShell::FAILURE));
+        }
+        EXIT_LOG;
+    }
+
+    void DeviceSettings::CallbackRevoked(const Core::IUnknown* remote, const uint32_t interfaceId)
+    {
+        // Add your handling code here, or leave empty if not needed
+        LOGINFO("CallbackRevoked called for interfaceId %u", interfaceId);
+    }
+
+} // namespace Plugin
+} // namespace WPEFramework
---
+++ DeviceSettings/DeviceSettingsFPDImplementation.cpp
@@ -0,0 +1,279 @@
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2024 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "DeviceSettingsFPDImplementation.h"
+
+#include "UtilsLogging.h"
+#include <syscall.h>
+
+using namespace std;
+
+namespace WPEFramework {
+namespace Plugin {
+
+    SERVICE_REGISTRATION(DeviceSettingsFPDImpl, 1, 0);
+
+    DeviceSettingsFPDImpl* DeviceSettingsFPDImpl::_instance = nullptr;
+
+    DeviceSettingsFPDImpl::DeviceSettingsFPDImpl()
+        : _fpd(FPD::Create(*this))
+    {
+        ENTRY_LOG;
+        DeviceSettingsFPDImpl::_instance = this;
+        LOGINFO("DeviceSettingsFPDImpl Constructor - Instance Address: %p", this);
+        EXIT_LOG;
+    }
+
+    DeviceSettingsFPDImpl::~DeviceSettingsFPDImpl() {
+        ENTRY_LOG;
+        LOGINFO("DeviceSettingsFPDImpl Destructor - Instance Address: %p", this);
+        EXIT_LOG;
+    }
+
+
+    template<typename Func, typename... Args>
+    void DeviceSettingsFPDImpl::dispatchFPDEvent(Func notifyFunc, Args&&... args) {
+        LOGINFO(">>");
+        _callbackLock.Lock();
+        for (auto& notification : _FPDNotifications) {
+            auto start = std::chrono::steady_clock::now();
+            (notification->*notifyFunc)(std::forward<Args>(args)...);
+            auto elapsed = std::chrono::steady_clock::now() - start;
+            LOGINFO("client %p took %" PRId64 "ms to process IFPD event", notification, std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count());
+        }
+        _callbackLock.Unlock();
+        LOGINFO("<<");
+    }
+
+    template <typename T>
+    Core::hresult DeviceSettingsFPDImpl::Register(std::list<T*>& list, T* notification)
+    {
+        uint32_t status = Core::ERROR_GENERAL;
+        ENTRY_LOG;
+        ASSERT(nullptr != notification);
+
+        _callbackLock.Lock();
+        // Make sure we can't register the same notification callback multiple times
+        if (std::find(list.begin(), list.end(), notification) == list.end()) {
+            list.push_back(notification);
+            notification->AddRef();
+            status = Core::ERROR_NONE;
+        } else {
+            LOGWARN("Notification %p already registered - skipping", notification);
+        }
+        _callbackLock.Unlock();
+
+        EXIT_LOG;
+        return status;
+    }
+
+    template <typename T>
+    Core::hresult DeviceSettingsFPDImpl::Unregister(std::list<T*>& list, const T* notification)
+    {
+        uint32_t status = Core::ERROR_GENERAL;
+        ENTRY_LOG;
+        ASSERT(nullptr != notification);
+        _callbackLock.Lock();
+
+        // Make sure we can't unregister the same notification callback multiple times
+        auto itr = std::find(list.begin(), list.end(), notification);
+        if (itr != list.end()) {
+            (*itr)->Release();
+            list.erase(itr);
+            status = Core::ERROR_NONE;
+        }
+
+        _callbackLock.Unlock();
+        EXIT_LOG;
+        return status;
+    }
+
+    Core::hresult DeviceSettingsFPDImpl::Register(DeviceSettingsFPD::INotification* notification)
+    {
+        ENTRY_LOG;
+        Core::hresult errorCode = Register(_FPDNotifications, notification);
+        if (errorCode != Core::ERROR_NONE) {
+            LOGERR("IFPD %p, errorCode: %u", notification, errorCode);
+        } else {
+            LOGINFO("IFPD %p registered successfully", notification);
+        }
+        EXIT_LOG;
+        return errorCode;
+    }
+
+    Core::hresult DeviceSettingsFPDImpl::Unregister(DeviceSettingsFPD::INotification* notification)
+    {
+        ENTRY_LOG;
+        Core::hresult errorCode = Unregister(_FPDNotifications, notification);
+        if (errorCode != Core::ERROR_NONE) {
+            LOGERR("IFPD %p, errorcode: %u", notification, errorCode);
+        } else {
+            LOGINFO("IFPD %p unregistered successfully", notification);
+        }
+        EXIT_LOG;
+        return errorCode;
+    }
+
+    // FPD notification implementation
+    void DeviceSettingsFPDImpl::OnFPDTimeFormatChanged(const FPDTimeFormat timeFormat)
+    {
+        LOGINFO("OnFPDTimeFormatChanged event Received: timeFormat=%d", timeFormat);
+        dispatchFPDEvent(&DeviceSettingsFPD::INotification::OnFPDTimeFormatChanged, timeFormat);
+    }
+
+    //Depricated
+    Core::hresult DeviceSettingsFPDImpl::SetFPDTime(const FPDTimeFormat timeFormat, const uint32_t minutes, const uint32_t seconds) {
+        ENTRY_LOG;
+        LOGINFO("SetFPDTime: timeFormat=%d, minutes=%u, seconds=%u", timeFormat, minutes, seconds);
+        EXIT_LOG;
+        return Core::ERROR_NONE;
+    }
+
+    Core::hresult DeviceSettingsFPDImpl::SetFPDScroll(const uint32_t scrollHoldDuration, const uint32_t nHorizontalScrollIterations, const uint32_t nVerticalScrollIterations) {
+        ENTRY_LOG;
+        LOGINFO("SetFPDScroll: scrollHoldDuration=%u, horizontal=%u, vertical=%u", scrollHoldDuration, nHorizontalScrollIterations, nVerticalScrollIterations);
+        EXIT_LOG;
+        return Core::ERROR_NONE;
+    }
+
+    Core::hresult DeviceSettingsFPDImpl::SetFPDTextBrightness(const FPDTextDisplay textDisplay, const uint32_t brightNess) {
+        ENTRY_LOG;
+        LOGINFO("SetFPDTextBrightness: textDisplay=%d, brightNess=%u", textDisplay, brightNess);
+        EXIT_LOG;
+        return Core::ERROR_NONE;
+    }
+
+    Core::hresult DeviceSettingsFPDImpl::GetFPDTextBrightness(const FPDTextDisplay textDisplay, uint32_t &brightNess) {
+        ENTRY_LOG;
+        brightNess = 50; // Example value
+        LOGINFO("GetFPDTextBrightness: textDisplay=%d", textDisplay);
+        EXIT_LOG;
+        return Core::ERROR_NONE;
+    }
+
+    Core::hresult DeviceSettingsFPDImpl::EnableFPDClockDisplay(const bool enable) {
+        ENTRY_LOG;
+        LOGINFO("EnableFPDClockDisplay: enable=%s", enable ? "true" : "false");
+        EXIT_LOG;
+        return Core::ERROR_NONE;
+    }
+
+    Core::hresult DeviceSettingsFPDImpl::GetFPDTimeFormat(FPDTimeFormat &fpdTimeFormat) {
+        ENTRY_LOG;
+        fpdTimeFormat = FPDTimeFormat::DS_FPD_TIMEFORMAT_24_HOUR; // Example value
+        EXIT_LOG;
+        return Core::ERROR_NONE;
+    }
+
+    Core::hresult DeviceSettingsFPDImpl::SetFPDTimeFormat(const FPDTimeFormat fpdTimeFormat) {
+        ENTRY_LOG;
+        LOGINFO("SetFPDTimeFormat: fpdTimeFormat=%d", fpdTimeFormat);
+        EXIT_LOG;
+        return Core::ERROR_NONE;
+    }
+
+    Core::hresult DeviceSettingsFPDImpl::SetFPDBlink(const FPDIndicator indicator, const uint32_t blinkDuration, const uint32_t blinkIterations) {
+        ENTRY_LOG;
+        LOGINFO("SetFPDBlink: indicator=%d, blinkDuration=%u, blinkIterations=%u", indicator, blinkDuration, blinkIterations);
+        EXIT_LOG;
+        return Core::ERROR_NONE;
+    }
+
+    Core::hresult DeviceSettingsFPDImpl::SetFPDMode(const FPDMode fpdMode) {
+        ENTRY_LOG;
+        LOGINFO("SetFPDMode: fpdMode=%d", fpdMode);
+        EXIT_LOG;
+        return Core::ERROR_NONE;
+    }
+    //Depricated
+
+    Core::hresult DeviceSettingsFPDImpl::SetFPDBrightness(const FPDIndicator indicator, const uint32_t brightNess, const bool persist) {
+        ENTRY_LOG;
+        LOGINFO("SetFPDBrightness: indicator=%d, brightNess=%u, persist=%s", indicator, brightNess, persist ? "true" : "false");
+
+        _apiLock.Lock();
+        _fpd.SetFPDBrightness(indicator, brightNess, persist);
+        _apiLock.Unlock();
+
+        EXIT_LOG;
+        return Core::ERROR_NONE;
+    }
+
+    Core::hresult DeviceSettingsFPDImpl::GetFPDBrightness(const FPDIndicator indicator, uint32_t &brightNess) {
+        ENTRY_LOG;
+
+        _apiLock.Lock();
+        _fpd.GetFPDBrightness(indicator, brightNess);
+        _apiLock.Unlock();
+
+        LOGINFO("GetFPDBrightness: indicator=%d, brightNess=%d", indicator, brightNess);
+        EXIT_LOG;
+        return Core::ERROR_NONE;
+    }
+
+    Core::hresult DeviceSettingsFPDImpl::SetFPDState(const FPDIndicator indicator, const FPDState state) {
+        ENTRY_LOG;
+        LOGINFO("SetFPDState: indicator=%d, state=%d", indicator, state);
+
+        _apiLock.Lock();
+        _fpd.SetFPDState(indicator, state);
+        _apiLock.Unlock();
+
+        EXIT_LOG;
+        return Core::ERROR_NONE;
+    }
+
+    Core::hresult DeviceSettingsFPDImpl::GetFPDState(const FPDIndicator indicator, FPDState &state) {
+        ENTRY_LOG;
+
+        _apiLock.Lock();
+        _fpd.GetFPDState(indicator, state);
+        _apiLock.Unlock();
+
+        LOGINFO("GetFPDState: indicator=%d, state=%d", indicator, state);
+        EXIT_LOG;
+        return Core::ERROR_NONE;
+    }
+
+    Core::hresult DeviceSettingsFPDImpl::GetFPDColor(const FPDIndicator indicator, uint32_t &color) {
+        ENTRY_LOG;
+
+        _apiLock.Lock();
+        _fpd.GetFPDColor(indicator, color);
+        _apiLock.Unlock();
+
+        LOGINFO("GetFPDColor: indicator=%d, color=0x%X", indicator, color);
+        EXIT_LOG;
+        return Core::ERROR_NONE;
+    }
+
+    Core::hresult DeviceSettingsFPDImpl::SetFPDColor(const FPDIndicator indicator, const uint32_t color) {
+        ENTRY_LOG;
+        LOGINFO("SetFPDColor: indicator=%d, color=0x%X", indicator, color);
+
+        _apiLock.Lock();
+        _fpd.SetFPDColor(indicator, color);
+        _apiLock.Unlock();
+
+        EXIT_LOG;
+        return Core::ERROR_NONE;
+    }
+
+} // namespace Plugin
+} // namespace WPEFramework

5bbce5622ac1052ee4fd41b23332d88f4cb2a1bf
Kirubagaran Jayaraman <kiruba115@gmail.com>
2026-06-08 14:54:36 -0400

    Kill all child processes started by ds-processes-load.sh

--- PowerManager/hal/DeepSleepImpl.h
+++ PowerManager/hal/DeepSleepImpl.h
@@ -143,7 +143,7 @@ public:
         if (WPEFramework::Core::ERROR_NONE == retCode) {
             LOGINFO("Device resumed from Deep sleep Mode, status :%s", str(status));
 
-            int killRet = system("sh -c \"for p in $(pgrep -f ds-processes-load.sh 2>/dev/null); do pkill -TERM -P $p >/dev/null 2>&1; pkill -KILL -P $p >/dev/null 2>&1; kill -9 $p >/dev/null 2>&1; done\"");
+            int killRet = system("for p in $(pgrep -f ds-processes-load.sh 2>/dev/null); do pkill -TERM -P $p >/dev/null 2>&1; pkill -KILL -P $p >/dev/null 2>&1; kill -9 $p >/dev/null 2>&1; done");
             if (killRet != 0) {
                 LOGINFO("Failed to fully stop ds-processes-load.sh process tree, return: %d", killRet);
             }
